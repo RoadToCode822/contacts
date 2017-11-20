d4f259fce3dcff240804cc21d5fdf2bf31d34ca9
Mahtab Alam <mahtab822@gmail.com>
2017-11-19 20:39:36 -0500

    Committed after Thursday, Nov. 15 lesson with Peter

--- controllers/contactsController.js
+++ controllers/contactsController.js
@@ -1,24 +1,24 @@
 var data = [
   {
     name: "John Doe",
-    phone: 416 888 8888,
+    phone: 4168888888,
     address: '26 lane street, toronto, ON'
   },
   {
     name: "Jane Doe",
-    phone: 416 232 2232,
+    phone: 4162322232,
     address: '1 Washington street, toronto, ON'
   },
   {
     name: "Jason Doe",
-    phone: 416 444 4444,
+    phone: 4164444444,
     address: '13 Bovaird street, toronto, ON'
   }
 ]
 
 module.exports = function(app){
   app.get('/contacts', function(req, res){
-    res.render('contacts', contactList: data);
+    res.render('contacts', {contacts: data});
   });
 
   app.post('/contacts', function(req, res){
